3f9022374cda98b4cdd12f36258dba95789f419e
Jaquesy <20432486+PassiveModding@users.noreply.github.com>
2023-08-29 17:29:41 +1000

    Redesign for demo structure

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_id" {
-  description = "The project ID to deploy to"
-  type        = string
-}
-
-variable "region" {
-  description = "The region to deploy to"
-  type        = string
-}
